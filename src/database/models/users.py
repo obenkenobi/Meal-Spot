eef0b19bc5015453e229fcf5c789eef1b59f3d00
OrenBen-Meir <orenbm11@gmail.com>
2019-11-28 00:51:15 -0500

    udated user model

--- src/database/models/users.py
+++ src/database/models/users.py
@@ -1,11 +1,11 @@
 from django.db import models
 from django.contrib.auth.models import User # This is so the built in user is added
 
-# Write down user models and information directly related to them here
+# Write down user models here
 
 # Abstract UserType model/class
 class UserType(models.Model):
-    userId = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
 
     class Meta: # Makes sure it is an abstract user model/class
         abstract = True
@@ -13,10 +13,19 @@ class UserType(models.Model):
 # Abstract Staff Class
 
 class Staff(UserType):
+    STATUS_CHOICES = [ # No need for status table, this also works
+        ('L', 'Laif Off'),
+        ('N', 'Not Hired'),
+        ('H', 'Hired')
+    ]
+    status = models.CharField(
+        max_length=2,
+        choices=STATUS_CHOICES,
+        default="N"
+    )
+    warnings = models.IntegerField(default=0)
     # TODO: Add the following
     # Foreign Key to Resturant must be added
-    # Foreign key to staff status or staff status field must be added
-    # Warning count must be added (Default value is 0)
 
     class Meta:
         abstract = True
@@ -25,25 +34,19 @@ class Staff(UserType):
 # Non-Staff user types
 
 class Customer(UserType):
-    # TODO: Add the following
-    # Order Count (Default is Zero)
-    # Foreign key to staff status or staff status field must be added
+    order_count = models.IntegerField(default=0)
     pass
 
 
 class Manager(UserType):
-    # TODO: Add the following
-    # Foreign Key to Resturant must be added
-    # Foreign key to staff status or staff status field must be added
+    # add oneToOne field for Resturant
     pass
 
 
 # Staff User Types
 
 class Cook(Staff):
-    # TODO: Add the following
-    # Food drop count 
-    pass
+    food_drop_count = models.IntegerField(default=0)
 
 class DeliveryPerson(Staff):
     pass # All needed fields are inhereted
