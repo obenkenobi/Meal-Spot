236857e1eff770aa9c12aa8be20cab83ebe5aeb5
fatsoflo <fatsoflo@users.noreply.github.com>
2019-12-07 14:50:31 -0500

    edit manager app

--- src/manager/urls.py
+++ src/manager/urls.py
@@ -1,4 +1,12 @@
 from django.urls import path
 from . import views
 
-urlpatterns = []
+urlpatterns = [
+    path('', views.home, name='manager-home'),
+    path('deliverybids', views.deliverybids, name='manager-deliverybids'),
+    path('pendingregistrations', views.pendingregistrations, name='manager-pendingregistrations'),
+    path('hirestaff', views.hirestaff, name='manager-hirestaff'),
+    path('restaurant', views.restaurant, name='manager-restaurant'),
+    path('staff', views.staff, name='manager-staff'),
+    path('customers', views.customers, name='manager-customers'),
+]
--- src/manager/views.py
+++ src/manager/views.py
@@ -2,47 +2,46 @@ from django.db import models
 from django.shortcuts import render, redirect
 from database.models.restaurant import *
 from database.models.user import *
-from helper import parse_req_body
+from helper import parse_req_body, userTypeChecker
 # Create your views here.
 
 def home(request):
-    if request.method == 'POST':
-        # redirect to the links?
-        if request.path == 'restaurant':
-            return redirect('restaurant')
-        elif request.path == 'delivery_bids':
-            return redirect('delivery_bids')
-        elif request.path == 'staff':
-            return redirect('staff')
-        elif request.path == 'customers':
-            return redirect('customers')
-        elif request.path == 'hire':
-            return redirect('hire')
-        elif request.path == 'pending_registrations':
-            return redirect('pending_registrations')
-    else:
-       return render(request, 'manager-home.html')
+    try:
+        userIs = userTypeChecker(request.user)
+        if userIs(user.Manager) == True: # returns true if user is manager, else false
+            return render(request, 'manager-home.html')
+        else:
+            return redirect('home-nexus')
+    except Exception as e:
+        print(e)
+        return redirect('home-nexus')
 
 def restaurant(request):
-    if request.method == 'POST':
-        body = parse_req_body(request.body)
-        name = body['name']
-        phonenumber = body['phonenumber']
-        description = body['description']
-        current_user = request.user
-        update_restaurant = Restaurant.objects.get(manager=current_user)
-        update_restaurant.name = name
-        update_restaurant.phonenumber = phonenumber
-        update_restaurant.description = description
-        update_restaurant.save()
+    try:
+        userIs = userTypeChecker(request.user)
+        if userIs(user.Manager) == True: # returns true if user is manager, else false
+            if request.method == 'POST':
+                body = parse_req_body(request.body)
+                name = body['name']
+                description = body['description']
+                current_user = request.user
+                update_restaurant = Restaurant.objects.get(manager=current_user)
+                update_restaurant.name = name
+                update_restaurant.phonenumber = phonenumber
+                update_restaurant.description = description
+                update_restaurant.save()
 
-    current_user = request.user
-    restaurant = Restaurant.objects.get(manager=current_user)
-    context = {'restaurant': restaurant}
-    return render(request, 'manager/restaurant.html', context=context)
+            current_user = request.user
+            restaurant = Restaurant.objects.get(manager=current_user)
+            context = {'restaurant': restaurant}
+            return render(request, 'manager/restaurant.html', context=context)
+        else:
+            return redirect('home-nexus')
+    except Exception as e:
+        print(e)
+        return redirect('home-nexus')
 
-
-def delivery_bids(request):
+def deliverybids(request):
     # at event that manager selects bid
     if request.method == 'POST':
         lowest_bid = DeliveryBid.objects.get(pk=delivery_bid_id)
@@ -74,8 +73,9 @@ def staff(request, staff_id):
     context = { 'staff': staff }
     return render(request, 'manager/staff.html', context=context)
 
-def hire_staff(request, staff_id):   
+def hirestaff(request, staff_id):   
     if request.method == 'POST':
+        
         update_staff = Staff.objects.get(user=staff_id)
         
         if request.body == 'hire':
@@ -105,7 +105,7 @@ def customers(request, customer_id):
     context = { 'customers': customers }
     return render(request, 'customers.html', context=context)
 
-def pending_registrations(request, customer_id):
+def pendingregistrations(request, customer_id):
     if request.method == 'POST':
         customer_status = CustomerStatus.objects.get(pk=customer_id)
         customer_status.approve_status()
