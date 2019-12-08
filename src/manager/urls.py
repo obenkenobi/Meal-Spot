5d4c41ab8ffe129ff4a0b0a2a45dfb6c75ae3178
fatsoflo <fatsoflo@users.noreply.github.com>
2019-12-07 20:10:53 -0500

    edit manager views

--- src/manager/urls.py
+++ src/manager/urls.py
@@ -5,8 +5,9 @@ urlpatterns = [
     path('', views.home, name='manager-home'),
     path('deliverybids', views.deliverybids, name='manager-deliverybids'),
     path('pendingregistrations', views.pendingregistrations, name='manager-pendingregistrations'),
-    path('hirestaff', views.hirestaff, name='manager-hirestaff'),
     path('restaurant', views.restaurant, name='manager-restaurant'),
     path('staff', views.staff, name='manager-staff'),
+    path('staff-details/<int:pk>', views.staff_details, name='manager-staff_details'),
     path('customers', views.customers, name='manager-customers'),
+    path('customer-details/<int:pk>', views.customer_details, name='manager-customer_details'),
 ]
