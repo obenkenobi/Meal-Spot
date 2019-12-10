8e1fd230e8fbcc8bd90efb0387f2e8e1ce933f8b
Oren Ben-Meir <orenbm11@gmail.com>
2019-12-09 22:10:27 -0500

    fixed signup bug

--- src/home/views.py
+++ src/home/views.py
@@ -40,52 +40,68 @@ def signup(request):
     """
     signup
     """
+    states = [choice[1] for choice in Address.STATE_CHOICES]
+    error = None
+
     if(request.method == "POST"):
-        print("testing signup")
         body = parse_req_body(request.body)
-        print(body)
         new_user = User.objects.create_user(body['usrname'], body['email'].replace('%', '@'), body['psw'])
-        new_user.save()
-        print(new_user.id)
+
+        addr_acceptable = '' in [ body['city'], body['state'], body['zipcode'] ]
+        addr_acceptable = not addr_acceptable
+        rest_acceptable = body['restname'] != ''
+
         if(body['usertype']=='cust'):
-            new_customer = Customer(user=new_user)
-            new_customer.save()
-            new_customer_address = CustomerAddress(
-                street_address = body['staddr'],
-                apt = body['apt'],
-                city = body['city'],
-                state = body['state'],
-                zip_code = body['zipcode'],
-                customer = new_customer
-            )
-            new_customer_address.save()
+            if addr_acceptable:
+                new_user.save()
+                new_customer = Customer(user=new_user)
+                new_customer.save()
+                new_customer_address = CustomerAddress(
+                    street_address = body['staddr'],
+                    apt = body['apt'],
+                    city = body['city'],
+                    state = body['state'],
+                    zip_code = int(body['zipcode']),
+                    customer = new_customer
+                )
+                new_customer_address.save()
+            else:
+                error = 'must enter address'
+                return render(request, 'home/signup.html', {'states': states, 'error': error})
         elif(body['usertype']=='mang'):
-            new_manager = Manager(user=new_user)
-            new_manager.save()
-            new_resturant = Restaurant(name=body['restname'], manager=new_manager)
-            new_resturant.save()
-            new_resturant_address = RestaurantAddress(
-                street_address = body['staddr'],
-                apt = body['apt'],
-                city = body
-                ['city'],
-                state = body['state'],
-                zip_code = body['zipcode'],
-                restaurant = new_resturant
-            )
-            new_resturant_address.save()
+            if addr_acceptable and rest_acceptable:
+                new_user.save()
+                new_manager = Manager(user=new_user)
+                new_manager.save()
+                new_resturant = Restaurant(name=body['restname'], manager=new_manager)
+                new_resturant.save()
+                new_resturant_address = RestaurantAddress(
+                    street_address = body['staddr'],
+                    apt = body['apt'],
+                    city = body
+                    ['city'],
+                    state = body['state'],
+                    zip_code = body['zipcode'],
+                    restaurant = new_resturant
+                )
+                new_resturant_address.save()
+            else:
+                error = 'must enter address and restaurant name'
+                return render(request, 'home/signup.html', {'states': states, 'error': error})
         elif(body['usertype']=='cook'):
+            new_user.save()
             new_staff = Cook(user=new_user)
             new_staff.save()
         elif(body['usertype']=='salespers'):
+            new_user.save()
             new_staff = Salesperson(user=new_user)
             new_staff.save()
         elif(body['usertype']=='deliverer'):
+            new_user.save()
             new_staff = Deliverer(user=new_user)
             new_staff.save()
         response = redirect('home-nexus')
         return response
     else:
-        states = [choice[1] for choice in Address.STATE_CHOICES]
-        return render(request, 'home/signup.html', {'states': states})
+        return render(request, 'home/signup.html', {'states': states, 'error': error})
 
