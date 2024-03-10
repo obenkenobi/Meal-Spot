be60b493f86e39bbc8e9bab98cda67cc4db12c75
Oren Ben-Meir <46640034+obenkenobi@users.noreply.github.com>
2024-03-09 20:39:51 -0500

    Update tests.py

--- src/database/tests.py
+++ src/database/tests.py
@@ -1,3 +1,9 @@
 from django.test import TestCase
 
 # Create your tests here.
+
+class DatabaseTestCase(TestCase):
+  # todo: test models here
+   def setUp(self):
+     # Todo: clear db changes
+     pass
